f028b3d27a96d1cd974ed3f0aeae9ad118c20f99
joelparavara <joelparavara@gmail.com>
2020-06-27 21:36:35 +0530

    Initial Commit with Pods

---
+++ Techcrus-Chat-App/Controllers/ConversationViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Techcrus-Chat-App
+//
+//  Created by Joel Thomson on 6/17/20.
+//  Copyright © 2020 Techcrus Labs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Techcrus-Chat-App/Controllers/LoginViewController.swift
@@ -0,0 +1,30 @@
+//
+//  LoginViewController.swift
+//  Techcrus-Chat-App
+//
+//  Created by Joel Thomson on 6/27/20.
+//  Copyright © 2020 Techcrus Labs. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
